890bd4ecc4401e6072a75c23394d581f6a156b04
Multistarkalpak <105732959+Multistarkalpak@users.noreply.github.com>
2022-06-16 18:01:27 +0530

    address book class and display details

---
+++ address_book/address_book/Address_book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Adressbook1
+{
+
+        class Adressbook1
+        {
+
+
+            public List<Contact> people = new List<Contact>();
+            
+            public void Display()
+            {
+                foreach (var contact in people)
+                {
+                    Console.WriteLine($"Firstname \t{contact.Firstname}\n Lastname \t{contact.Lastname}\n PhoneNumber \t{contact.PhoneNumber}\n city \t{contact.city}\n Zip \t{contact.Zip}\n Email \t{contact.Email}\n Adress \t{contact.Adress}\n ");
+
+                }
+                Console.ReadLine();
+            }
+           
+        }
+    
+}
+
